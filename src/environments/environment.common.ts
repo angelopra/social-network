7e9abfcca20354025cc8ec1d136f4465ce8ae75d
Ângelo <angelo.pralves@gmail.com>
2024-08-12 22:10:22 -0300

    feat: environment variables

---
+++ src/environments/environment.common.ts
@@ -0,0 +1,30 @@
+const envCommon = {
+  apiRoutes: {
+    follow: {
+      request: '/follow/{followedId}',
+      stopFollowing: '/follow/{followedId}',
+      accept: '/follow/{followerId}/accept',
+      deny: '/follow/{followerId}/deny',
+    },
+    group: {
+      getById: '/group/{groupId}',
+      getPosts: '/group/{groupId}/posts',
+      create: '/group',
+    },
+    post: {
+      create: '/post',
+    },
+    tag: {
+      getAll: '/tag',
+    },
+    user: {
+      search: '/user',
+      getById: '/user/{userId}',
+      posts: '/user/{userId}/posts',
+      current: '/user/current',
+      feed: '/user/current/feed',
+    },
+  },
+};
+
+export default envCommon;
