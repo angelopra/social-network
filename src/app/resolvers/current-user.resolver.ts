b04070df16fe5ced457e2dcba8015a64a84aceb2
angelopra <85626081+angelopra@users.noreply.github.com>
2024-11-14 19:14:50 -0300

    update angular v15 -> v18 (#10)
    * patch: save token on session storage
    
    * update angular v15 -> v16
    
    * update angular v16 -> v17
    
    * update angular v17 -> v18

--- src/app/resolvers/current-user.resolver.ts
+++ src/app/resolvers/current-user.resolver.ts
@@ -1,9 +1,9 @@
 import { inject } from "@angular/core";
-import { ActivatedRouteSnapshot, ResolveFn, Router, RouterStateSnapshot } from "@angular/router";
-import { CurrentUserDto } from "../models";
-import { UserService } from "../services/user/user.service";
-import { LoadingService } from "../services/loading/loading.service";
+import { ActivatedRouteSnapshot, ResolveFn, RouterStateSnapshot } from "@angular/router";
 import { finalize } from "rxjs";
+import { CurrentUserDto } from "../models";
+import { LoadingService } from "../services/loading/loading.service";
+import { UserService } from "../services/user/user.service";
 
 export const currentUserResolver: ResolveFn<CurrentUserDto> = (route: ActivatedRouteSnapshot, _: RouterStateSnapshot) => {
   const userService = inject(UserService);
